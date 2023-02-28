64ec0b21c2740927ba89f70e2ba59b583e395c10
TaerangLee <s22051@gsm.hs.kr>
2023-02-28 22:27:29 +0900

    update early setting

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+
+export default function Router() {
+    return(
+        <Routes>
+            <Route path="/" element={<div/>}/>
+        </Routes>
+    );
+}
+
