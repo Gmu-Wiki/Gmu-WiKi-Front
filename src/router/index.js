6c9359268b1dde33e75f5e2702c48486a888fa8f
TaerangLee <s22051@gsm.hs.kr>
2023-03-02 17:57:08 +0900

    annotation

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,16 @@ import React from "react";
 function Router() {
     return(
         <Routes>
-            <Route path="/emailAuthentication" element={<EmailAuthentication />}/>
+            {/* <Route path="/emailAuthentication" element={<EmailAuthentication />}/>
             <Route path="/certificationNumber" element={<CertificationNumber />}/>
             <Route path="/joinPage" element={<JoinPage/>}/>
             <Route path="/loginPage" element={<LoginPage />}/>
-            <Route path="/findPasswordPage" element={<FindPasswordPage/>}/>
-            <Route path="/myPage" element={<MyPage />}/>
+            <Route path="/findPasswordPage" element={<FindPasswordPage/>}/> */}
+            {/* <Route path="/myPage" element={<MyPage />}/>
             <Route path="/writePage" element={<WritePage />}/>
             <Route path="/homePage" element={<HomePage />}/>
             <Route path="/noticePage" element={<NoticePage/>}/>
-            <Route path="/viewAll" element={<ViewAll />}/>
+            <Route path="/viewAll" element={<ViewAll />}/> */}
             
         </Routes>
     );
