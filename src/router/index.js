93b2fd5582a706abaab60931473baa23e13f1910
TaerangLee <s22051@gsm.hs.kr>
2023-03-02 17:58:09 +0900

    router setting renew

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
 
-
 function Router() {
-    return(
-        <Routes>
-            {/* <Route path="/emailAuthentication" element={<EmailAuthentication />}/>
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path="/emailAuthentication" element={<EmailAuthentication />}/>
             <Route path="/certificationNumber" element={<CertificationNumber />}/>
             <Route path="/joinPage" element={<JoinPage/>}/>
             <Route path="/loginPage" element={<LoginPage />}/>
             <Route path="/findPasswordPage" element={<FindPasswordPage/>}/> */}
-            {/* <Route path="/myPage" element={<MyPage />}/>
+        {/* <Route path="/myPage" element={<MyPage />}/>
             <Route path="/writePage" element={<WritePage />}/>
             <Route path="/homePage" element={<HomePage />}/>
             <Route path="/noticePage" element={<NoticePage/>}/>
             <Route path="/viewAll" element={<ViewAll />}/> */}
-            
-        </Routes>
-    );
+      </Routes>
+    </Router>
+  );
 }
 
 export default Router;
-
