421913e1e235fe73553404566f4e683ae0a87aa6
TaerangLee <s22051@gsm.hs.kr>
2023-03-02 16:21:58 +0900

    first setting

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,24 @@
 import { Routes, Route } from "react-router-dom";
+import React from "react";
 
 
-export default function Router() {
+function Router() {
     return(
         <Routes>
-            <Route path="/" element={<div/>}/>
+            <Route path="/emailAuthentication" element={<EmailAuthentication />}/>
+            <Route path="/certificationNumber" element={<CertificationNumber />}/>
+            <Route path="/joinPage" element={<JoinPage/>}/>
+            <Route path="/loginPage" element={<LoginPage />}/>
+            <Route path="/findPasswordPage" element={<FindPasswordPage/>}/>
+            <Route path="/myPage" element={<MyPage />}/>
+            <Route path="/writePage" element={<WritePage />}/>
+            <Route path="/homePage" element={<HomePage />}/>
+            <Route path="/noticePage" element={<NoticePage/>}/>
+            <Route path="/viewAll" element={<ViewAll />}/>
+            
         </Routes>
     );
 }
+
+export default Router;
 
