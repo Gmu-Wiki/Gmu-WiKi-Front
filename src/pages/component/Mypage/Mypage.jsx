1ce12ffc5a3b32b205ab1883717dce914b94f373
TaerangLee <s22051@gsm.hs.kr>
2023-03-05 03:15:31 +0900

    update the styled UserContent

--- src/pages/component/Mypage/Mypage.jsx
+++ src/pages/component/Mypage/Mypage.jsx
@@ -6,22 +6,33 @@ import { faMagnifyingGlass, faBars } from "@fortawesome/free-solid-svg-icons";
 
 function MyPage() {
   return (
-    <S.Header>
-      <S.HeaderList>
-        <S.LogoImg src="https://media.discordapp.net/attachments/956190154454876183/1080807476515045376/image.png" />
-        <S.MenuList>
-          <span>홈</span>
-          <span className="myPageMenu">마이페이지</span>
-          <span>글쓰기</span>
-          <span>공지</span>
-          <span>전체보기</span>
-        </S.MenuList>
-        <S.SideBar>
-          <FontAwesomeIcon icon={faMagnifyingGlass} className="readingGlasses"/>
-          <FontAwesomeIcon icon={faBars} className="bars"/>
-        </S.SideBar>
-      </S.HeaderList>
-    </S.Header>
+    <>
+      <S.Header>
+        <S.HeaderList>
+          <S.LogoImg src="https://media.discordapp.net/attachments/956190154454876183/1080807476515045376/image.png" />
+          <S.MenuList>
+            <span>홈</span>
+            <span className="myPageMenu">마이페이지</span>
+            <span>글쓰기</span>
+            <span>공지</span>
+            <span>전체보기</span>
+          </S.MenuList>
+          <S.SideBar>
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              className="readingGlasses"
+            />
+            <FontAwesomeIcon icon={faBars} className="bars" />
+          </S.SideBar>
+        </S.HeaderList>
+      </S.Header>
+      <S.ContentCenter>
+        <div className="UserContent">
+          <S.UserList></S.UserList>
+        </div>
+        <S.ArticlesList></S.ArticlesList>
+      </S.ContentCenter>
+    </>
   );
 }
 
--- src/pages/component/Mypage/style.js
+++ src/pages/component/Mypage/style.js
@@ -5,7 +5,6 @@ export const Header = styled.div`
   width: 100%;
   height: 90px;
   background-color: white;
-  position: fixed;
   display: flex;
   align-items: center;
 `;
@@ -20,6 +19,8 @@ export const HeaderList = styled.div`
 export const LogoImg = styled.img`
   height: 30px;
   cursor: pointer;
+  position:relative;
+  left: 50px;
 `;
 
 export const MenuList = styled.div`
@@ -34,6 +35,7 @@ export const MenuList = styled.div`
   .myPageMenu {
     color: #ffb800;
   }
+ 
 `;
 
 export const SideBar = styled.div`
@@ -57,4 +59,37 @@ export const SideBar = styled.div`
       transform: scale(1.05);
     }
   }
+
+  position:relative;
+  right: 50px;
+`;
+
+export const ContentCenter = styled.div`
+  .UserContent{
+    width: 35%;
+    height: 100%;
+    display:flex;
+    position:relative;
+    left: 90px;
+    justify-content: center;
+  }
+  height: 100%;
+  display:flex
+`;
+
+export const UserList = styled.div`
+  margin-top: 65px;
+  width: 48%;
+  background-color:white;
+  height: 34vh;
+  border-radius: 13px;
+`;
+
+export const ArticlesList = styled.div`
+    width: 44%;
+    height: 77vh;
+    background-color: white;
+    border-radius: 13px;
+    margin-top: 65px;
+
 `;
