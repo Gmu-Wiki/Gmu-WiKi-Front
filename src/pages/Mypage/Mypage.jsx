fa4475c0b81ff7448428a67caf568d90d1632064
TaerangLee <s22051@gsm.hs.kr>
2023-03-05 23:03:13 +0900

    update Header -> components

--- src/pages/Mypage/Mypage.jsx
+++ src/pages/Mypage/Mypage.jsx
@@ -3,29 +3,13 @@ import React, { Component } from "react";
 import "./index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass, faBars } from "@fortawesome/free-solid-svg-icons";
+import Header from "../component/Header";
+
 
 function MyPage() {
   return (
     <>
-      <S.Header>
-        <S.HeaderList>
-          <S.LogoImg src="https://media.discordapp.net/attachments/956190154454876183/1080807476515045376/image.png" />
-          <S.MenuList>
-            <span>홈</span>
-            <span className="myPageMenu">마이페이지</span>
-            <span>글쓰기</span>
-            <span>공지</span>
-            <span>전체보기</span>
-          </S.MenuList>
-          <S.SideBar>
-            <FontAwesomeIcon
-              icon={faMagnifyingGlass}
-              className="readingGlasses"
-            />
-            <FontAwesomeIcon icon={faBars} className="bars" />
-          </S.SideBar>
-        </S.HeaderList>
-      </S.Header>
+      <Header />
       <S.ContentCenter>
         <div className="UserContent">
           <S.UserList>
@@ -33,7 +17,7 @@ function MyPage() {
           </S.UserList>
         </div>
         <S.ArticlesList>
-            
+
         </S.ArticlesList>
       </S.ContentCenter>
     </>
---
+++ src/pages/component/Header.js
@@ -0,0 +1,32 @@
+import * as S from "../Mypage/style";
+import "../Mypage/index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faBars } from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+  return (
+    <>
+      <S.Header>
+        <S.HeaderList>
+          <S.LogoImg src="https://media.discordapp.net/attachments/956190154454876183/1080807476515045376/image.png" />
+          <S.MenuList>
+            <span>홈</span>
+            <span className="myPageMenu">마이페이지</span>
+            <span>글쓰기</span>
+            <span>공지</span>
+            <span>전체보기</span>
+          </S.MenuList>
+          <S.SideBar>
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              className="readingGlasses"
+            />
+            <FontAwesomeIcon icon={faBars} className="bars" />
+          </S.SideBar>
+        </S.HeaderList>
+      </S.Header>
+    </>
+  );
+};
+
+export default Header;
