d2b2872f88c2e2405a43320fb773552a00d3bdd1
TaerangLee <s22051@gsm.hs.kr>
2023-03-06 15:23:54 +0900

    styled UserList

--- src/pages/Mypage/Mypage.jsx
+++ src/pages/Mypage/Mypage.jsx
@@ -12,10 +12,23 @@ function MyPage() {
       <S.ContentCenter>
         <div className="UserContent">
           <S.UserList>
-
+            <div className="UserImg">
+              <S.UserImg src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIMsQAbRYXh_RRV-65pHol_qOnijspiBbwSg&usqp=CAU"/>
+              <div className="UserImformation">
+                <span>0000 김하온</span>
+                <span className="email">s22060@gsm.hs.kr</span>
+              </div>
+            </div>
+            <S.ButtonList>
+              <S.LogOut>로그아웃</S.LogOut>
+              <S.ProfileOption>프로필편집</S.ProfileOption>
+              
+            </S.ButtonList>
           </S.UserList>
         </div>
-        <S.ArticlesList></S.ArticlesList>
+        <S.ArticlesList>
+
+        </S.ArticlesList>
       </S.ContentCenter>
     </>
   );
--- src/pages/Mypage/style.js
+++ src/pages/Mypage/style.js
@@ -82,6 +82,34 @@ export const UserList = styled.div`
   background-color: white;
   height: 34vh;
   border-radius: 13px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  .UserImg {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 25px;
+
+    span {
+      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+        Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+      position: relative;
+    }
+
+    .email {
+      color: #b2b2b2;
+    }
+  }
+
+
+  .UserImformation{
+    margin-top: 13px;
+    align-items:center;
+    display:flex;
+    flex-direction:column;
+  }
 `;
 
 export const ArticlesList = styled.div`
@@ -92,3 +120,38 @@ export const ArticlesList = styled.div`
   border-radius: 13px;
   margin-top: 65px;
 `;
+
+export const UserImg = styled.img`
+  width: 6rem;
+  height: 6rem;
+  border-radius: 3rem;
+  cursor: pointer;
+`;
+
+export const ButtonList = styled.div`
+  width: 100%;
+  height:100%;
+  display:flex;
+  align-items:flex-end;
+  justify-content:center;
+`;
+
+export const LogOut = styled.button`
+  color: #b2b2b2;
+  background-color:white;
+  border: 1px solid #b2b2b2;
+  border-radius: 8px;
+  width: 85px;
+  height: 33px;
+  cursor: pointer;
+`;
+
+export const ProfileOption = styled.button`
+  color:white;
+  background-color:#ffb800;
+  width: 85px;
+  height: 33px;
+  border-radius: 8px;
+  border:0;
+  cursor: pointer;
+`
