c1a48de2475e082a039cbba2457fce2d59cc7028
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-03-12 21:16:53 +0900

    update styled components

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React from 'react'; //테스트
-import MyPage from '../pages/Mypage/Mypage';
-import WritePage from '../pages/WritePage/WritePage';
-import ErrorPage from '../pages/404ErrorPage/ErrorPage';
-import NoticePage from '../pages/Notice/NoticePage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MyPage from "../pages/MyPage/Mypage";
+import WritePage from "../pages/WritePage/WritePage";
+import ErrorPage from "../pages/404ErrorPage/ErrorPage";
+import NoticePage from "../pages/Notice/NoticePage.jsx";
+import React from "react";
 
 function RouterApp() {
   return (
@@ -11,12 +11,12 @@ function RouterApp() {
       <Routes>
         {/* <Route path="/emailAuthentication" element={<EmailAuthentication />} />
         <Route path="/certificationNumber" element={<CertificationNumber />} />
-        <Route path="/join" element={<JoinPage />} />
+        <Route path="/join" elemlement={<NoticePage />} />
+        <Route path="/viewAllent={<JoinPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/findPasswordPage" element={<FindPasswordPage />} />
         <Route path="/home" element={<HomePage />} />
-        <Route path="/notice" element={<NoticePage />} />
-        <Route path="/viewAll" element={<ViewAll />} /> */}
+        <Route path="/notice" e" element={<ViewAll />} /> */}
 
         <Route path="/write" element={<WritePage />} />
         <Route path="/*" element={<ErrorPage />} />
